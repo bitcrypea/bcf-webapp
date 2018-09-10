704082deb6da4c8cbdd6fc0b3f99c181c747a396
Tô Thành Thương <tothuongthanh@gmail.com>
2018-09-10 22:17:36 +0700

    update config

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,9 @@
-const config = {
+let config = {
   graphqlEndpoint: 'https://api-stag.bitcrypea.com/graphql',
 };
+
+if (process.env.NODE_ENV === 'production') {
+  config.graphqlEndpoint = 'https://api.bitcrypea.com/graphql';
+}
 
 export default config;
