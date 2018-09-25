f9b18df9c59ec8eec0f455a150c502623e81f60b
Tô Thành Thương <tothuongthanh@gmail.com>
2018-09-25 21:03:29 +0700

    update link

--- src/redux/tokens.js
+++ src/redux/tokens.js
@@ -3,14 +3,20 @@ import jwt from 'jsonwebtoken'
 const authTokenFromStorage = () => {
   const tokenId = localStorage.getItem('TOKEN_ID');
   const tokenSecret = localStorage.getItem('TOKEN_SECRET');
-
-  var payload = {
-    sub: tokenId,
-    jti: Date.now(),
-  };
-
-  var token = jwt.sign(payload, tokenSecret);
-  return token;
+  console.log(tokenId);
+  console.log(tokenSecret);
+  if (tokenId !== null && tokenSecret !== null)
+  {
+    var payload = {
+      sub: tokenId,
+      jti: Date.now(),
+    };
+  
+    var token = jwt.sign(payload, tokenSecret);
+    return token;
+  }
+  
+  return null;
 };
 
 export { authTokenFromStorage };
